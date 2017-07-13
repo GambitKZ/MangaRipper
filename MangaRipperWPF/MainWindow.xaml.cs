039f6391327e8adebfedc635e3cb044228022c21
Gambit <rusnigdrag@gmail.com>
2017-07-13 23:59:58 +0600

    Make the carcase in WPF

---
+++ MangaRipperWPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using NLog;
+using System.Windows;
+
+namespace MangaRipperWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public MainWindow()
+        {
+            Logger.Info("> Main()");
+            InitializeComponent();
+            Logger.Info("< Main()");
+        }
+    }
+}
